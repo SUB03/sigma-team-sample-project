1ab30642c8af319a1680185ca5626ef630755ef6
Данило Бобрицький <34156g@gmail.com>
2025-12-11 18:09:13 +0000

    admin django init

--- backend/UserService/user/models.py
+++ backend/UserService/user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=150, unique=True, null=False, blank=False)
     email = models.EmailField(unique=True, null=False, blank=False)
     password = models.CharField(max_length=128, null=False, blank=False)
