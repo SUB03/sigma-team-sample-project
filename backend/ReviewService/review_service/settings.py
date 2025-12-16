90b46f5668ff6e8b3cb616dda71a09dc86b5a9cb
Данило Бобрицький <34156g@gmail.com>
2025-12-16 22:06:41 +0000

    fix

--- backend/ReviewService/review_service/settings.py
+++ backend/ReviewService/review_service/settings.py
@@ -116,3 +116,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = os.environ.get('CORS_ALLOW_ALL_ORIGINS', 'True') == 'True'
+CORS_ALLOW_CREDENTIALS = True
